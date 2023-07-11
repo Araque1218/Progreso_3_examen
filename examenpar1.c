34a08bf9d123e32b6e5545032e5012fa8861d05b
Mateo_Araque34 <131192048+Araque1218@users.noreply.github.com>
2023-07-11 12:20:22 -0500

    Primera parte

---
+++ examenpar1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+
+int corregirPalabras (FILE *textoEntrada, FILE, *textoSalida,) 
+   char palabra [50]
+   int todaslaspalabrascorregidas = 0
+
+ printf("Ingrese el texto de entrada: ");
+    scanf("%s", nombretextoEntrada);
+
+printf("Ingrese el texto de salida; ");
+    scanf("%s", nombretextoSalida);
+
+
+
